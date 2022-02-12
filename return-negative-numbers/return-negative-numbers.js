13cd528c7c2ebb33284ca73cc8c1d164f5c08ef9
Tan Matthews <tancodinggeneration@gmail.com>
2022-02-12 16:09:55 +0000

    turning negative to positive numbers

--- return-negative-numbers/return-negative-numbers.js
+++ return-negative-numbers/return-negative-numbers.js
@@ -1,3 +1,15 @@
 function makeNegative(num) {
-  // Code?
+  //can use the -Maths.abs
+  // * positive num by -1
+  // if num is positive so more than 0 return -num
+
+  if (num > 0) {
+    return -num;
+  } else {
+    return num;
+  }
 }
+
+//return -Math.abs(num);
+
+//return -num;
