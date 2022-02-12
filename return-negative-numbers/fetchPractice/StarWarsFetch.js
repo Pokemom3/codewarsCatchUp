d7692c2071594a9854b22ae12a743ae86e899279
Tan Matthews <tancodinggeneration@gmail.com>
2022-02-12 00:04:10 +0000

    reordered folders and added plan notes

--- return-negative-numbers/fetchPractice/StarWarsFetch.js
+++ return-negative-numbers/fetchPractice/StarWarsFetch.js
@@ -1,21 +1,19 @@
-// const getPlanets = async function getPlanets() {
-//   const response = await fetch("https://swapi.dev/api//planets/3/");
-
-//   fetch("https://swapi.dev/api//planets/3/").then((response) => {});
-
-//   console.log(response);
-// };
-// getPlanets(response);
-
 console.log("StarWars rules");
 
+//Get a quote from an API in an ASYNC function
+//await it
 async function getPlanets() {
   const response = await fetch(`https://swapi.dev/api//planets/`);
   console.log(response);
+  //without await is stuck in a Promise. A promise is pending and not enough
+  //AWAIT the response in a variable
+  //the response comes back in json format so .json
   const data = await response.json();
   console.log(data);
+  //pull the data that we need from the object using dot notation.
   let StarWarsPlanets = data.results;
   console.log(StarWarsPlanets);
 }
 
+//call the function outside of the function!
 getPlanets();
